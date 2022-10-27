d53ea866f497cc86f62755daef82017e783a2a11
Rakibul Hasan <39860864+rakibhasantusar@users.noreply.github.com>
2022-10-27 16:07:42 +0600

    finally ready for deploy alhamdulillah

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000 // port where i hit data
 //for cross origin
 app.use(cors())
 
